4d4c6e7891b7ddf085a65126856202a968e8f333
Jaeho <kumoh2@naver.com>
2024-02-12 20:26:44 +0100

    [#5]scenarioMessage UI create

---
+++ JHEquipSim/JHEquipSim/Views/scenarioMessage.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace JHEquipSim.Views
+{
+    partial class scenarioMessage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            sendXmlButton = new Button();
+            treeView1 = new TreeView();
+            btn_add_treexml = new Button();
+            btn_remove_treexml = new Button();
+            btn_rename = new Button();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            richTextBox1.Location = new Point(209, 29);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(586, 418);
+            richTextBox1.TabIndex = 3;
+            richTextBox1.Text = "";
+            // 
+            // sendXmlButton
+            // 
+            sendXmlButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            sendXmlButton.Location = new Point(720, 0);
+            sendXmlButton.Name = "sendXmlButton";
+            sendXmlButton.Size = new Size(75, 23);
+            sendXmlButton.TabIndex = 2;
+            sendXmlButton.Text = "Send";
+            sendXmlButton.UseVisualStyleBackColor = true;
+            sendXmlButton.Click += sendXmlButton_Click;
+            // 
+            // treeView1
+            // 
+            treeView1.Location = new Point(0, 29);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(203, 418);
+            treeView1.TabIndex = 4;
+            treeView1.NodeMouseDoubleClick += TreeView1_NodeMouseDoubleClick;
+            // 
+            // btn_add_treexml
+            // 
+            btn_add_treexml.Location = new Point(3, 3);
+            btn_add_treexml.Name = "btn_add_treexml";
+            btn_add_treexml.Size = new Size(25, 23);
+            btn_add_treexml.TabIndex = 5;
+            btn_add_treexml.Text = "+";
+            btn_add_treexml.UseVisualStyleBackColor = true;
+            btn_add_treexml.Click += btn_add_treexml_Click;
+            // 
+            // btn_remove_treexml
+            // 
+            btn_remove_treexml.Location = new Point(29, 3);
+            btn_remove_treexml.Name = "btn_remove_treexml";
+            btn_remove_treexml.Size = new Size(25, 23);
+            btn_remove_treexml.TabIndex = 6;
+            btn_remove_treexml.Text = "-";
+            btn_remove_treexml.UseVisualStyleBackColor = true;
+            btn_remove_treexml.Click += btn_remove_treexml_Click;
+            // 
+            // btn_rename
+            // 
+            btn_rename.Location = new Point(60, 3);
+            btn_rename.Name = "btn_rename";
+            btn_rename.Size = new Size(75, 23);
+            btn_rename.TabIndex = 7;
+            btn_rename.Text = "name edit";
+            btn_rename.UseVisualStyleBackColor = true;
+            btn_rename.Click += btn_rename_Click;
+            // 
+            // singleMessage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btn_rename);
+            Controls.Add(btn_remove_treexml);
+            Controls.Add(btn_add_treexml);
+            Controls.Add(treeView1);
+            Controls.Add(richTextBox1);
+            Controls.Add(sendXmlButton);
+            Name = "singleMessage";
+            Size = new Size(800, 450);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private Button sendXmlButton;
+        private TreeView treeView1;
+        private Button btn_add_treexml;
+        private Button btn_remove_treexml;
+        private Button btn_rename;
+    }
+}
---
+++ JHEquipSim/JHEquipSim/Views/scenarioMessage.cs
@@ -0,0 +1,72 @@
+﻿using JHEquipSim.Helpers;
+using JHEquipSim.ServiceReference;
+using System.Xml.Linq;
+
+namespace JHEquipSim.Views
+{
+    public partial class scenarioMessage : UserControl
+    {
+        string rootPath = Path.Combine(Application.StartupPath, "xml");
+
+        public scenarioMessage()
+        {
+            InitializeComponent();
+            TreeViewHelper.InitializeTreeView(treeView1, rootPath);
+        }
+
+        private void TreeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Node.Tag != null && Path.GetExtension(e.Node.Tag.ToString()).Equals(".xml", StringComparison.OrdinalIgnoreCase))
+            {
+                try
+                {
+                    // XDocument를 사용하여 XML 파일을 불러오고 서식을 지정합니다.
+                    var xDocument = XDocument.Load(e.Node.Tag.ToString());
+                    string formattedXml = xDocument.ToString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"XML 파일을 불러오는 중 오류가 발생했습니다: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private async void sendXmlButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = new XmlReceiverServiceClient();
+                await client.ReceiveXmlAsync(richTextBox1.Text);
+                MessageBox.Show("XML 데이터가 서버로 전송되었습니다.", "전송 성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                await client.CloseAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"서버로의 전송 중 오류가 발생했습니다: {ex.Message}", "전송 오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_add_treexml_Click(object sender, EventArgs e)
+        {
+            // 새 그룹 폴더 추가 또는 선택된 그룹에 XML 파일 추가
+            if (treeView1.SelectedNode != null && treeView1.SelectedNode.Parent == null)
+            {
+                TreeViewHelper.AddNewGroupFolder(treeView1, rootPath);
+            }
+            else
+            {
+                TreeViewHelper.AddXmlFileToGroup(treeView1, rootPath);
+            }
+        }
+
+        private void btn_remove_treexml_Click(object sender, EventArgs e)
+        {
+            TreeViewHelper.RemoveSelectedItem(treeView1, rootPath);
+        }
+
+        private void btn_rename_Click(object sender, EventArgs e)
+        {
+            TreeViewHelper.RenameSelectedNode(treeView1, rootPath);
+        }
+    }
+}
