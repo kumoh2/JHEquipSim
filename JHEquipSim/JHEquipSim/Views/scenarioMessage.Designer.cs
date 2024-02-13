f398bf4303217e0e56bcd2b99fcfce9674258ca4
Jaeho <kumoh2@naver.com>
2024-02-13 05:41:59 +0100

    [#5]scenarioMessage Detail UI create

--- JHEquipSim/JHEquipSim/Views/scenarioMessage.Designer.cs
+++ JHEquipSim/JHEquipSim/Views/scenarioMessage.Designer.cs
@@ -28,27 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            richTextBox1 = new RichTextBox();
             sendXmlButton = new Button();
             treeView1 = new TreeView();
             btn_add_treexml = new Button();
             btn_remove_treexml = new Button();
             btn_rename = new Button();
+            comboBox1 = new ComboBox();
+            btn_add_scenario = new Button();
+            listView1 = new ListView();
+            button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
-            // 
-            // richTextBox1
-            // 
-            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            richTextBox1.Location = new Point(209, 29);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(586, 418);
-            richTextBox1.TabIndex = 3;
-            richTextBox1.Text = "";
             // 
             // sendXmlButton
             // 
             sendXmlButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            sendXmlButton.Location = new Point(720, 0);
+            sendXmlButton.Location = new Point(720, 3);
             sendXmlButton.Name = "sendXmlButton";
             sendXmlButton.Size = new Size(75, 23);
             sendXmlButton.TabIndex = 2;
@@ -94,28 +89,78 @@
             btn_rename.UseVisualStyleBackColor = true;
             btn_rename.Click += btn_rename_Click;
             // 
-            // singleMessage
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(209, 4);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 8;
+            // 
+            // btn_add_scenario
+            // 
+            btn_add_scenario.Location = new Point(336, 4);
+            btn_add_scenario.Name = "btn_add_scenario";
+            btn_add_scenario.Size = new Size(88, 23);
+            btn_add_scenario.TabIndex = 9;
+            btn_add_scenario.Text = "add scenario";
+            btn_add_scenario.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(207, 29);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(588, 418);
+            listView1.TabIndex = 10;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(425, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 23);
+            button1.TabIndex = 11;
+            button1.Text = "edit global value";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(533, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(106, 23);
+            button2.TabIndex = 12;
+            button2.Text = "edit param value";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // scenarioMessage
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listView1);
+            Controls.Add(btn_add_scenario);
+            Controls.Add(comboBox1);
             Controls.Add(btn_rename);
             Controls.Add(btn_remove_treexml);
             Controls.Add(btn_add_treexml);
             Controls.Add(treeView1);
-            Controls.Add(richTextBox1);
             Controls.Add(sendXmlButton);
-            Name = "singleMessage";
+            Name = "scenarioMessage";
             Size = new Size(800, 450);
             ResumeLayout(false);
         }
 
         #endregion
-
-        private RichTextBox richTextBox1;
         private Button sendXmlButton;
         private TreeView treeView1;
         private Button btn_add_treexml;
         private Button btn_remove_treexml;
         private Button btn_rename;
+        private ComboBox comboBox1;
+        private Button btn_add_scenario;
+        private ListView listView1;
+        private Button button1;
+        private Button button2;
     }
 }
--- JHEquipSim/JHEquipSim/Views/scenarioMessage.cs
+++ JHEquipSim/JHEquipSim/Views/scenarioMessage.cs
@@ -16,34 +16,10 @@ namespace JHEquipSim.Views
 
         private void TreeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
         {
-            if (e.Node.Tag != null && Path.GetExtension(e.Node.Tag.ToString()).Equals(".xml", StringComparison.OrdinalIgnoreCase))
-            {
-                try
-                {
-                    // XDocument를 사용하여 XML 파일을 불러오고 서식을 지정합니다.
-                    var xDocument = XDocument.Load(e.Node.Tag.ToString());
-                    string formattedXml = xDocument.ToString();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"XML 파일을 불러오는 중 오류가 발생했습니다: {ex.Message}", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
         }
 
         private async void sendXmlButton_Click(object sender, EventArgs e)
         {
-            try
-            {
-                var client = new XmlReceiverServiceClient();
-                await client.ReceiveXmlAsync(richTextBox1.Text);
-                MessageBox.Show("XML 데이터가 서버로 전송되었습니다.", "전송 성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                await client.CloseAsync();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"서버로의 전송 중 오류가 발생했습니다: {ex.Message}", "전송 오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
         }
 
         private void btn_add_treexml_Click(object sender, EventArgs e)
